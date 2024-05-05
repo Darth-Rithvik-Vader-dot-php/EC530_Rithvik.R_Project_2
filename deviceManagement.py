c76c3f77a0f5789fe33cff35f962830ce7d3c7ac
Darth-Rithvik-Vader-dot-php <144507773+Darth-Rithvik-Vader-dot-php@users.noreply.github.com>
2024-05-05 00:47:44 -0400

    Update deviceManagement.py
    Inside commits done, details changes done

--- deviceManagement.py
+++ deviceManagement.py
@@ -4,23 +4,20 @@ from bson import ObjectId, json_util
 import json
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://visaaln2:visaal456@ec530.qevqtrc.mongodb.net/healthmonitoring?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://rithvikr88:rithvikr@ec530.qevqtrc.mongodb.net/healthmonitoring?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-# Custom JSON serializer for ObjectId
 def custom_json_serializer(obj):
     if isinstance(obj, ObjectId):
         return str(obj)
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
-# Updated to use MongoDB's json_util for handling BSON types
 def jsonify_with_objectid(data):
     return json.loads(json_util.dumps(data))
 
 @app.route('/devices', methods=['POST'])
 def register_device():
     data = request.json
-    # Ensure required fields are in the request
     if not all(k in data for k in ("deviceType", "manufacturer", "model", "status")):
         return jsonify({'error': 'Missing required fields'}), 400
     if data['status'] not in ["active", "inactive"]:
@@ -63,4 +60,3 @@ def submit_device_data(deviceId):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
